689d71943a03050a35524b1b87e2883b5253fbb2
Michael Harrison <michael@ntechmedia.com>
2018-01-14 16:11:10 +1000

    Added bands model

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+let Band = EmberObject.extend({
+  name: '',
+});
 
 export default Route.extend({
+  model: function () {
+    let ledZeppelin = Band.create({name: 'Led Zeppelin'});
+    let pearlJam = Band.create({name: 'Pearl Jam'});
+    let fooFighters = Band.create({name: 'Foo Fighters'});
+
+    return [ledZeppelin, pearlJam, fooFighters];
+  }
 });
