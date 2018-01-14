2beb9c393f62ca15ecd8cf1f507586cc2aaea252
Michael Harrison <michael@ntechmedia.com>
2018-01-14 13:46:49 +1000

    Added songs

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
 
 export default Route.extend({
+  model: function () {
+    let blackDog = EmberObject.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+
+    let yellowLedbetter = EmberObject.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+
+    let pretender = EmberObject.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    return [blackDog, yellowLedbetter, pretender];
+  }
 });
