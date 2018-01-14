c73c7f3ade1122814265a416654409306ccc8516
Michael Harrison <michael@ntechmedia.com>
2018-01-14 18:11:49 +1000

    Added songs in routes/bands.js

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -5,11 +5,39 @@ let Band = EmberObject.extend({
   name: '',
 });
 
+let Song = EmberObject.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
 export default Route.extend({
   model: function () {
-    let ledZeppelin = Band.create({name: 'Led Zeppelin'});
-    let pearlJam = Band.create({name: 'Pearl Jam'});
-    let fooFighters = Band.create({name: 'Foo Fighters'});
+    let blackDog = Song.create({
+      title: 'Black Dog',
+      band: 'Led Zeppelin',
+      rating: 3
+    });
+    let yellowLedbetter = Song.create({
+      title: 'Yellow Ledbetter',
+      band: 'Pearl Jam',
+      rating: 4
+    });
+    let pretender = Song.create({
+      title: 'The Pretender',
+      band: 'Foo Fighters',
+      rating: 2
+    });
+
+    let daughter = Song.create({
+      title: 'Daughter',
+      band: 'Pearl Jam',
+      rating: 5
+    });
+
+    let ledZeppelin = Band.create({name: 'Led Zeppelin', songs: [blackDog]});
+    let pearlJam = Band.create({name: 'Pearl Jam', songs: [yellowLedbetter, daughter]});
+    let fooFighters = Band.create({name: 'Foo Fighters', songs: [pretender]});
 
     return [ledZeppelin, pearlJam, fooFighters];
   }
