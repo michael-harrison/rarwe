a5a1eed3ca8e64b73d6d4ab3cf9074af6523e04a
Michael Harrison <michael@ntechmedia.com>
2018-01-27 15:06:17 +1000

    Added rendering of star ratings

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -1,4 +1,24 @@
+import {computed} from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
+  tagName: 'div',
+  classNames: ['rating-panel'],
+
+  rating: 0,
+  maxRating: 5,
+
+  stars: computed('rating', 'maxRating', function () {
+    let fullStars = this.starRange(1, this.get('rating'), 'full');
+    let emptyStars = this.starRange(this.get('rating') + 1, this.get('maxRating'), 'empty');
+    return fullStars.concat(emptyStars);
+  }),
+
+  starRange: function (start, end, type) {
+    let starsData = [];
+    for (let i = start; i <= end; i++) {
+      starsData.push({rating: i, full: type === 'full'});
+    }
+    return starsData;
+  }
 });
