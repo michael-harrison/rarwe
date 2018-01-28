1b0c01d7d16545afb64f96dd9c44df62c796d70b
Michael Harrison <michael@ntechmedia.com>
2018-01-28 10:07:55 +1000

    Added transitionTo to move to song creation directly after band has been created Added no songs holding page

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,6 +1,10 @@
+import {computed} from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  noSongs: computed('model.songs.[]', function () {
+    return this.get('model.songs.length') === 0;
+  }),
   actions: {
     updateRating: function (params) {
       let song = params.item, rating = params.rating;
