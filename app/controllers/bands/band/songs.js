8fbcf36e693dec73e555d2bcb96d9f69ae310c9c
Michael Harrison <michael@ntechmedia.com>
2018-01-28 10:21:08 +1000

    Changed display so that "There are no songs yet. Why don't you create one?" is hidden after the 'create one?' link is clicked

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -2,10 +2,14 @@ import {computed} from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  noSongs: computed('model.songs.[]', function () {
-    return this.get('model.songs.length') === 0;
+  songCreationStarted: false,
+  canCreateSong: computed('songCreationStarted', 'model.songs.[]', function () {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
   actions: {
+    enableSongCreation: function () {
+      this.set('songCreationStarted', true);
+    },
     updateRating: function (params) {
       let song = params.item, rating = params.rating;
 
