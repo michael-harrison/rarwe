d05d1349c8774df3030618629b5f8c55f1a6db4f
Michael Harrison <michael@ntechmedia.com>
2018-01-28 14:05:09 +1000

    Added details page for rendering of band details

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,17 @@
+import {isEmpty} from '@ember/utils';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
   model: function (params) {
     let bands = this.modelFor('bands');
     return bands.findBy('slug', params.slug); // params.slug is now 'pearl-jam'
+  },
+  afterModel: function (band) {
+    let description = band.get('description');
+    if (isEmpty(description)) {
+      this.transitionTo('bands.band.songs');
+    } else {
+      this.transitionTo('bands.band.details');
+    }
   }
 });
