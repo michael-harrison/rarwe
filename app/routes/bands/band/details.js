7cb091002253b4f422fcfd12b8df5110a8697af2
Michael Harrison <michael@ntechmedia.com>
2018-02-09 09:35:01 +1000

    Added warning when navigating away from the description editing

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -3,5 +3,20 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model: function () {
     return this.modelFor('bands.band');
+  },
+  actions: {
+    willTransition: function (transition) {
+      let controller = this.get('controller');
+      let leave;
+
+      if (controller.get('isEditing')) {
+        leave = window.confirm("You have unsaved changes. Are you sure you want to leave?");
+        if (leave) {
+          controller.set('isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    }
   }
 });
